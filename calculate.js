bcecbb3017ebdd2208bbd421dbac2157ad988b5b
dorinengabdoh <ngabdohdorine@gmail.com>
2023-08-23 08:29:48 -0400

    sum of array element

---
+++ calculate.js
@@ -0,0 +1,22 @@
+'use strict'
+let first=document.getElementById('cont1');
+let last=document.getElementById('cont2');
+let conver=document.getElementById('conver');
+// fonction
+function sum(arr){
+  let somme =0;
+for (let i=0; i<arr.length; i++){
+  somme=somme+arr[i];
+}
+  return somme;
+}
+
+// display
+
+function display(){
+ first = first.value.split(',').map(Number);
+ const all= sum(first);
+ last.value =`${all}`;
+}
+conver.addEventListener('click', display);
+
